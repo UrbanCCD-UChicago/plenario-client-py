a7e8deb8643a12335db3823b0bddf9b3ca60dc0e
Sanil Sampat <sanilio@gmail.com>
2018-07-27 09:16:35 -0500

    base for client

---
+++ plenario_client/client.py
@@ -0,0 +1,17 @@
+import config
+import requests
+from plenario_response.response import PlenarioResponse
+
+
+class PlenarioClient(object):
+
+    def __init__(self, api_url=None):
+        if api_url is None:
+            api_url = config.API_URL
+        self.api_url = api_url
+        self.session = requests.Session()
+        self.response = None
+
+    def get(self):
+        response = (self.session.get(self.api_url))
+        return PlenarioResponse(response.json())
