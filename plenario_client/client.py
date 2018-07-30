0a58553dbfb6368b9b7bbca64187b358b2ccfb32
Sanil Sampat <sanilio@gmail.com>
2018-07-30 16:39:45 -0500

    annotations

--- plenario_client/client.py
+++ plenario_client/client.py
@@ -4,12 +4,13 @@ from plenario_response.response import PlenarioResponse
 
 DEFAULT_API_URL = config.API_URL
 
+
 class PlenarioClient:
 
     def __init__(self, api_url: str=DEFAULT_API_URL):
-        self.api_url = api_url
-        self.session = requests.Session()
-        self.response = None
+        self.api_url: str = api_url
+        self.session: requests.Session = requests.Session()
+        self.response: requests.Response = None
 
     def get(self) -> PlenarioResponse:
         self.response = self.session.get(self.api_url)
