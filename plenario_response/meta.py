c7a3d6eab302598f652358a6079f397b7de936aa
Sanil Sampat <sanilio@gmail.com>
2018-07-30 14:50:58 -0500

    annotations

--- plenario_response/meta.py
+++ plenario_response/meta.py
@@ -1,28 +1,28 @@
-class Meta(object):
+class Meta:
 
-    def __init__(self, json_payload_dict):
-        self.payload = json_payload_dict
-        self.params = self.payload.get('params')
-        self.links = self.payload.get('links')
-        self.counts = self.payload.get('counts')
+    def __init__(self, json_payload: dict):
+        self.payload: dict = json_payload
+        self.params: dict = self.payload.get('params')
+        self.links: dict = self.payload.get('links')
+        self.counts: dict = self.payload.get('counts')
 
-    def get_page(self):
+    def get_page(self) -> int:
+        return self.params.get('page')
+
+    def get_page_size(self) -> int:
         return self.params.get('page_size')
 
-    def get_page_size(self):
-        return self.params.get('page_size')
-
-    def get_current_page_link(self):
+    def get_current_page_link(self) -> str:
         return self.links.get('current')
 
-    def get_next_page_link(self):
-        return self.links.get('current')
+    def get_next_page_link(self) -> str:
+        return self.links.get('next')
 
-    def get_previous_link(self):
+    def get_previous_link(self) -> str:
         return self.links.get('previous')
 
-    def get_params(self):
+    def get_params(self) -> dict:
         return self.params
 
-    def get_counts(self):
+    def get_counts(self) -> dict:
         return self.counts
--- plenario_response/response.py
+++ plenario_response/response.py
@@ -3,6 +3,9 @@ from plenario_response.meta import Meta
 
 class PlenarioResponse:
 
-    def __init__(self, json_payload_dict):
-        self.payload = json_payload_dict
-        self.meta = Meta(self.payload.get('meta'))
+    def __init__(self, json_payload: dict):
+        self.payload: dict = json_payload
+        self.meta: Meta = Meta(self.payload.get('meta'))
+
+    def get_meta(self) -> Meta:
+        return self.meta
