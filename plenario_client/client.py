19252133457a47aabb3e48ef7014097b0c5fbb38
Sanil Sampat <sanilio@gmail.com>
2018-07-30 14:48:55 -0500

    set the default api url to config url

--- plenario_client/client.py
+++ plenario_client/client.py
@@ -2,16 +2,15 @@ import config
 import requests
 from plenario_response.response import PlenarioResponse
 
+DEFAULT_API_URL = config.API_URL
 
-class PlenarioClient(object):
+class PlenarioClient:
 
-    def __init__(self, api_url=None):
-        if api_url is None:
-            api_url = config.API_URL
+    def __init__(self, api_url: str=DEFAULT_API_URL):
         self.api_url = api_url
         self.session = requests.Session()
         self.response = None
 
-    def get(self):
+    def get(self) -> PlenarioResponse:
         self.response = self.session.get(self.api_url)
         return PlenarioResponse(self.response.json())
