163bd8c79dbb0c19d46b26e02f83691c5f0959b3
Sanil Sampat <sanilio@gmail.com>
2018-08-08 13:39:05 -0500

    test meta response

--- tests/test_meta_response.py
+++ tests/test_meta_response.py
@@ -2,31 +2,33 @@ import pytest
 import json
 from plenario_response.meta import Meta
 
-PAGE = 1
-PAGE_SIZE = 500
+PARAMS = {
+    "page_size": 500,
+    "page": 1
+}
 COUNTS = {
-    "total_records": 4290,
+    "total_records": 4381,
     "total_pages": 9,
     "errors": 0,
     "data": 500
 }
 LINKS = {
     "previous": None,
-    "next": "http://localhost:4000/api/v2/data-sets/chicago-beach-lab-dna-tests?inserted_at=le%3A2018-08-02T18%3A39%3A13.368679&page_size=500&page=2",
-    "current": "http://localhost:4000/api/v2/data-sets/chicago-beach-lab-dna-tests?inserted_at=le%3A2018-08-02T18%3A39%3A13.368679&page_size=500&page=1"
+    "next": "http://localhost:4000/api/v2/data-sets/chicago-beach-lab-data-dna-tests?page_size=500&page=2",
+    "current": "http://localhost:4000/api/v2/data-sets/chicago-beach-lab-data-dna-tests?page_size=500&page=1"
 }
 
 
 @pytest.fixture
 def load_json():
-    with open("fixtures/chicago_beach_lab_dna_tests") as f:
+    with open("tests/fixtures/chicago_beach_lab_dna_tests_page_one.json") as f:
         return json.load(f)
 
 
 def test_meta_response():
     meta = Meta(load_json())
-    assert meta.page == PAGE
-    assert meta.page_size == PAGE_SIZE
+    assert meta.page == PARAMS.get("page")
+    assert meta.page_size == PARAMS.get("page_size")
     assert meta.counts == COUNTS
     assert meta.current_page_link == LINKS.get("current")
     assert meta.previous_link == LINKS.get("previous")
