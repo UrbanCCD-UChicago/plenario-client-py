6cb266ba9d3b0187bf57c79b979e1a49fa84b05b
Sanil Sampat <sanilio@gmail.com>
2018-08-08 15:26:43 -0500

    start fleshing out data class

--- plenario_response/data.py
+++ plenario_response/data.py
@@ -1,2 +1,12 @@
 class Data:
-    pass
+
+    def __init__(self, json_payload: dict):
+        self._data: dict = json_payload
+
+    @property
+    def data(self) -> dict:
+        return self._data
+
+    @property
+    def count(self) -> int:
+        return len(self._data)
