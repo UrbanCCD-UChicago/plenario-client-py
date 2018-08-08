a6b4af1719ee64a238c7896135fb30b7fb17d839
Sanil Sampat <sanilio@gmail.com>
2018-08-08 13:37:15 -0500

    private vars

--- plenario_response/meta.py
+++ plenario_response/meta.py
@@ -1,35 +1,40 @@
 class Meta:
 
     def __init__(self, json_payload: dict):
-        self.payload: dict = json_payload
-        self.params: dict = self.payload.get('params')
-        self.links: dict = self.payload.get('links')
-        self.counts: dict = self.payload.get('counts')
+        self.payload = json_payload.get('meta')
+        self._params: dict = self.payload.get('params')
+        self._links: dict = self.payload.get('links')
+        self._counts: dict = self.payload.get('counts')
 
     @property
     def page(self) -> int:
-        return self.params.get('page')
+        return self._params.get('page')
 
     @property
     def page_size(self) -> int:
-        return self.params.get('page_size')
+        return self._params.get('page_size')
 
     @property
     def current_page_link(self) -> str:
-        return self.links.get('current')
+        return self._links.get('current')
 
     @property
     def next_page_link(self) -> str:
-        return self.links.get('next')
+        return self._links.get('next')
 
     @property
     def previous_link(self) -> str:
-        return self.links.get('previous')
+        return self._links.get('previous')
 
     @property
     def params(self) -> dict:
-        return self.params
+        return self._params
 
     @property
     def counts(self) -> dict:
-        return self.counts
+        return self._counts
+
+    @property
+    def links(self) -> dict:
+        return self._links
+
