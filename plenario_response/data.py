555b8d0f44f1d00d58c59a0090316cc30b7926ca
Sanil Sampat <sanilio@gmail.com>
2018-08-09 11:01:04 -0500

    dataset

--- plenario_response/data.py
+++ plenario_response/data.py
@@ -1,12 +1,12 @@
 class Data:
 
     def __init__(self, json_payload: dict):
-        self._data: dict = json_payload
+        self._dataset: dict = json_payload
 
     @property
-    def data(self) -> dict:
-        return self._data
+    def dataset(self) -> dict:
+        return self._dataset
 
     @property
     def count(self) -> int:
-        return len(self._data)
+        return len(self._dataset)
